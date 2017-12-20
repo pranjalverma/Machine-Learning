050de443a6a737b3f08958e32bc81e430a331cfa
Pranjal Verma <pranjalverma.wiki@gmail.com>
2017-12-20 17:18:23 +0530

    Update predictOneVsAll.m

--- ex3 -MNIST and Forward Propagation in Neural Networks/predictOneVsAll.m
+++ ex3 -MNIST and Forward Propagation in Neural Networks/predictOneVsAll.m
@@ -28,7 +28,7 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
+%
 
 [Y, p] = max(sigmoid(X*all_theta'), [], 2);
 
