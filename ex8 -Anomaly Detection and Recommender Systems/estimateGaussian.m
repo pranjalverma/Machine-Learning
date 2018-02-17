5fe69982804c709153da32bcbf65e035e689b3d6
Pranjal Verma <pranjalverma.wiki@gmail.com>
2018-02-17 16:00:19 +0530

    Update estimateGaussian.m

--- ex8 -Anomaly Detection and Recommender Systems/estimateGaussian.m
+++ ex8 -Anomaly Detection and Recommender Systems/estimateGaussian.m
@@ -14,7 +14,7 @@ function [mu sigma2] = estimateGaussian(X)
 mu = zeros(n, 1);
 sigma2 = zeros(n, 1);
 
-% ====================== YOUR CODE HERE ======================
+% ====================== YOUR CODE HERE =====================
 % Instructions: Compute the mean of the data and the variances
 %               In particular, mu(i) should contain the mean of
 %               the data for the i-th feature and sigma2(i)
@@ -24,7 +24,7 @@ sigma2 = zeros(n, 1);
 mu = mean(X, 1)';
 sigma2 = (1/m)*sum((X' - mu).^2, 2);
 
-% =============================================================
+% ============================================================
 
 
 end
